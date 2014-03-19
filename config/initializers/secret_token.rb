2945c459a614fa5a46b2804128ecf980fbab09d8
James Gabriel <james.s.gabriel@gmail.com>
2014-03-18 20:15:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collabevent::Application.config.secret_key_base = '5a7de9371aab7854ad3e03ba01a2131c07fbf30e08237a7f94377f687fd8224aafa9d39a8bdbc328f2a9142f7c8039d956bee127eca941ca14e8018a8285f356'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Collabevent::Application.config.session_store :cookie_store, key: '_collabevent_session'
