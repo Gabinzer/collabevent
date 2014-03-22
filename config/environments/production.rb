ca318b7e6eebe60c86c221b3a2903b7b06c69009
James Gabriel <james.s.gabriel@gmail.com>
2014-03-22 19:00:44 -0400

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Collabevent::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku - change to actual host name when available
+  config.action_mailer.default_url_options = { :host => 'collabevent.herokuapp.com'}
+
 end
